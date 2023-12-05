5eb6b4e7cf5bc187bf5956aa0abe45b0b5081d70
Ashley Cao <ashleycao12@gmail.com>
2023-12-05 19:03:06 +1300

    inital commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-blue-400 text-3xl text-center mt-5">Nuxt 3 - Tailwind Boiler Plate</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
